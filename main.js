e46d644ccd209772e4aad0e0f29321c5f64eeaa1
juliagunneriusson <Guju23yc@student.ju.se>
2024-04-18 11:03:53 +0200

    update

--- main.js
+++ main.js
@@ -68,6 +68,13 @@ function displayListitems() {
   }
 }
 
+function updateTaskStyle(taskElement, isChecked) {
+  if (isChecked) {
+    taskElement.style.textDecoration = "line-through";
+  } else {
+    taskElement.style.textDecoration = "none";
+  }
+}
 
 function loadHandler() {
   const saveButton = document.getElementById("save");
